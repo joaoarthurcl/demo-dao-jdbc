3e634ff0c2536625db8baa7a3e9c4500fedcae4f
João Leandro <joaoarthurcl@gmail.com>
2020-02-27 00:15:14 +0000

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -38,6 +38,11 @@ public class Main {
 		sellerDao.insert(newSeller); //inserir no BD
 		System.out.println("Inserted! New Id = " + newSeller.getId()); //saber se trouxe o ID de volta
 		
+		System.out.println("\n======= TEST 5: seller update =======");
+		seller  = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Updated completed!");
 	}
 
 }
